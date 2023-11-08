8e42c8d1ead683c079c685888bda3099aa80ffc9
Mozzi02 <128518865+Mozzi02@users.noreply.github.com>
2023-11-08 18:41:34 -0300

    Delete dist directory

--- dist/shared/db/conn.mysql.js
+++
@@ -1,8 +0,0 @@
-import mysql from 'mysql2/promise';
-export const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'sistema_ventas_dsw'
-});
-//# sourceMappingURL=conn.mysql.js.map
